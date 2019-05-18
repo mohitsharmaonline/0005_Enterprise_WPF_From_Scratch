1dd79b0922ee9a626b94043c0e15cca739dfc689
Mohit Sharma <mohitsharmaonline@gmail.com>
2019-05-18 20:23:21 +0530

    Code changes for Async demo.

--- 0005_Asynchronous_Programming/AsynchronousDemo/AsynchronousDemo/MainWindow.xaml.cs
+++ 0005_Asynchronous_Programming/AsynchronousDemo/AsynchronousDemo/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿
+using System;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AsynchronousDemo
 {
@@ -23,6 +13,54 @@ namespace AsynchronousDemo
         public MainWindow()
         {
             InitializeComponent();
+            ComputeStuffAsync();
+        }
+
+        private async void ComputeStuffAsync()
+        {
+            for (;;)
+            {
+                var sum = 0.0;
+                Message.Text = "Computing...";
+                await Task.Run(() => 
+                {
+                    for(int i = 0; i < 200000000; i++)
+                    {
+                        sum += Math.Sqrt(i);
+                    }
+                });
+                Message.Text = "Sum = " + sum;
+
+                await Task.Run(() =>
+                {
+                    for (int i = 0; i < 200000000; i++)
+                    {
+                        sum += Math.Sqrt(i);
+                    }
+                });
+                Message.Text = "Sum = " + sum;
+                await Task.Run(() =>
+                {
+                    for (int i = 0; i < 200000000; i++)
+                    {
+                        sum += Math.Sqrt(i);
+                    }
+                });
+                Message.Text = "Sum = " + sum;
+                await Task.Run(() =>
+                {
+                    for (int i = 0; i < 200000000; i++)
+                    {
+                        sum += Math.Sqrt(i);
+                    }
+                });
+                Message.Text = "Sum = " + sum;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button Clicked!");
         }
     }
 }
